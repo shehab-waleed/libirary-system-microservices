debabfc0150849ef5d43bd8a4a12d31f23af8202
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-05-08 10:26:20 +0300

    chore: Update Vite server port to 3000

--- ReactServiceApp/vite.config.js
+++ ReactServiceApp/vite.config.js
@@ -9,6 +9,9 @@ const __dirname = dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		port: 3000,
+	},
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "./src"),
