ddddceeb7812f91d79905f8efbb022ed8e3c37b5
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2024-05-08 09:20:50 +0300

    Added missed aspect in main file in users service

--- UserServiceApp/src/main/java/com/example/UserServiceApp/UserServiceAppApplication.java
+++ UserServiceApp/src/main/java/com/example/UserServiceApp/UserServiceAppApplication.java
@@ -2,8 +2,10 @@ package com.example.UserServiceApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class UserServiceAppApplication {
 
 	public static void main(String[] args) {
